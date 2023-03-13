28a88439beaa89d4593c96ce17b4f880a88a33b8
vaibhav2569 <vaibhavkumar@cedcoss.com>
2023-03-13 19:58:04 +0530

    reduced 30 lines of code and resolved test cases

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,100 +1,75 @@
 function validate() {
-    // geting age from user
+  // geting age from user
   let age = document.getElementById("age");
-//   getting weight from user
+  //   getting weight from user
   let weight = document.getElementById("weight");
-//   message variable to display message
-  let message;
-//   err variable from dealing with negative values only
-  let err;
-  if (age.value < 0) {
-    err = "****Age cannot be negative***";
-    document.getElementById("ageError").innerHTML = err;
-  }
-  if (weight.value < 0) {
-    err = "***weight cannot be negative***";
-    document.getElementById("weightError").innerHTML = err;
-  }
+  //   message variable to display message
+  let message = [
+    "You are fit",
+    "overweight",
+    "underweight",
+    "Please enter the  age within range",
+    "Please enter the weight within range",
+  ];
+  var text = "";
+
   if (age.value >= 5 && age.value <= 7) {
     if (weight.value >= 15 && weight.value <= 20) {
-      message = "You are fit";
-      document.getElementById("message").innerHTML = message;
-      document.getElementById("message").style.color = "green";
+      text = message[0];
     } else if (weight.value > 20) {
-      message = "overweight";
-      document.getElementById("message").innerHTML = message;
-      document.getElementById("message").style.color = "red";
-    } else {
-      if (weight.value >= 0) {
-        message = "underweight";
-        document.getElementById("message").innerHTML = message;
-        document.getElementById("message").style.color = "red";
-      }
+      text = message[1];
+    } else  {
+        text = message[2];
     }
   }
-  if (age.value >= 8 && age.value <= 10) {
+  else if (age.value >= 8 && age.value <= 10) {
     if (weight.value >= 21 && weight.value <= 25) {
-      message = "You are fit";
-      document.getElementById("message").innerHTML = message;
-      document.getElementById("message").style.color = "green";
+      text = message[0];
     } else if (weight.value > 25) {
-      message = "overweight";
-      document.getElementById("message").innerHTML = message;
-      document.getElementById("message").style.color = "red";
+      text = message[1];
     } else {
       if (weight.value >= 0) {
-        message = "underweight";
-        document.getElementById("message").innerHTML = message;
-        document.getElementById("message").style.color = "red";
+        text = message[2];
       }
     }
   }
-  if (age.value >= 11 && age.value <= 15) {
+ else if (age.value >= 11 && age.value <= 15) {
     if (weight.value >= 26 && weight.value <= 30) {
-      message = "You are fit";
-      document.getElementById("message").innerHTML = message;
+      text = message[0];
       document.getElementById("message").style.color = "green";
     } else if (weight.value > 30) {
-      message = "overweight";
-      document.getElementById("message").innerHTML = message;
+      text = message[1];
       document.getElementById("message").style.color = "red";
     } else {
       if (weight.value >= 0) {
-        message = "underweight";
-        document.getElementById("message").innerHTML = message;
+        text = message[2];
         document.getElementById("message").style.color = "red";
       }
     }
   }
-  if (age.value >= 16 && age.value <= 20) {
+  else if (age.value >= 16 && age.value <= 20) {
     if (weight.value >= 31 && weight.value <= 40) {
-      message = "You are fit";
-      document.getElementById("message").innerHTML = message;
-      document.getElementById("message").style.color = "green";
+      text = message[0];
     } else if (weight.value > 40) {
-      message = "overweight";
-      document.getElementById("message").innerHTML = message;
-      document.getElementById("message").style.color = "red";
+      text = message[1];
     } else {
       if (weight.value >= 0) {
-        message = "underweight";
-        document.getElementById("message").innerHTML = message;
-        document.getElementById("message").style.color = "red";
+        text = message[2];
       }
     }
-  } else {
+  } 
+  else {
     if (age.value < 5 || age.value > 20) {
-      message = "Please enter the valid age";
-      document.getElementById("message").innerHTML = message;
-      document.getElementById("message").style.color = "red";
-    }
-
-    if (weight.value < 15 || weight.value > 40) {
-      message = "Please enter the valid weight";
-      document.getElementById("message").innerHTML = message;
-      document.getElementById("message").style.color = "red";
+      text = message[3];
+    } else if (weight.value < 15 || weight.value > 40) {
+      text = message[4];
     }
   }
-//   return false so that my output stays on the page
+  document.getElementById("message").innerHTML = text;
+  text === message[0]
+    ? (document.getElementById("message").style.color = "green")
+    : (document.getElementById("message").style.color = "red");
+
+  //   return false so that my output stays on the page
   return false;
 }
