4bd891b75fe451837aed5e34eaf735a30fa262bc
vaibhav2569 <vaibhavkumar@cedcoss.com>
2023-03-14 11:51:35 +0530

    resolved cases

--- src/JS/script.js
+++ src/JS/script.js
@@ -7,62 +7,65 @@ function validate() {
   let message = [
     "You are fit",
     "overweight",
-    "underweight",
-    "Please enter the  age within range",
-    "Please enter the weight within range",
+    "underweight"
   ];
-  var text = "";
-
-  if (age.value >= 5 && age.value <= 7) {
+  let text = "";
+   if(age.value==""|| weight.value=="")
+   {
+    text="please fill input";
+   } 
+  else if (age.value >= 5 && age.value <= 7) {
     if (weight.value >= 15 && weight.value <= 20) {
       text = message[0];
-    } else if (weight.value > 20) {
+    } else if (weight.value > 20 &&weight.value<=200) {
       text = message[1];
-    } else  {
+    } else if(weight.value<15 && weight.value>0) {
         text = message[2];
+    }
+    else{
+      text="Invalid weight";
     }
   }
   else if (age.value >= 8 && age.value <= 10) {
     if (weight.value >= 21 && weight.value <= 25) {
       text = message[0];
-    } else if (weight.value > 25) {
+    } else if (weight.value > 25 &&weight.value<=200) {
       text = message[1];
-    } else {
-      if (weight.value >= 0) {
+    } else if(weight.value<21 && weight.value>0){
         text = message[2];
-      }
+    }
+    else{
+      text="Invalid weight";
     }
   }
  else if (age.value >= 11 && age.value <= 15) {
     if (weight.value >= 26 && weight.value <= 30) {
       text = message[0];
-      document.getElementById("message").style.color = "green";
-    } else if (weight.value > 30) {
+    } else if (weight.value > 30 && weight.value<=200) {
       text = message[1];
-      document.getElementById("message").style.color = "red";
-    } else {
-      if (weight.value >= 0) {
+    } else if(weight.value<26 && weight.value>0){
         text = message[2];
-        document.getElementById("message").style.color = "red";
+      }
+      else{
+        text="Invalid weight";
       }
     }
-  }
+  
   else if (age.value >= 16 && age.value <= 20) {
     if (weight.value >= 31 && weight.value <= 40) {
       text = message[0];
-    } else if (weight.value > 40) {
+    } else if (weight.value > 40 && weight.value<=200) {
       text = message[1];
-    } else {
-      if (weight.value >= 0) {
-        text = message[2];
-      }
+    } else if(weight.value<31 && weight.value>0) {
+      text = message[2];
+    }
+    else{
+      text="Invalid weight";
     }
   } 
   else {
-    if (age.value < 5 || age.value > 20) {
-      text = message[3];
-    } else if (weight.value < 15 || weight.value > 40) {
-      text = message[4];
+    if(age.value<0 || age.value>21 ||weight.value<0 ||weight.value>200){
+      text="Invalid input";
     }
   }
   document.getElementById("message").innerHTML = text;
